2815c648e3e7e7fd07b330bd269fa4dce142a01b
JoelLee <yjocli@163.com>
2018-10-24 23:08:15 +0800

    Initial commit

---
+++ src/main/java/com/fkyy/ct/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.fkyy.ct.controller;
+
+import com.fkyy.ct.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	@RequestMapping("/{id}") 
+	public String  getUser(@PathVariable Integer id,Model model) {
+		
+		model.addAttribute("user",new User(id,"张三",20,"中国广州"));
+		return "/user/detail";
+	}
+
+	@RequestMapping("/list")
+	public String  listUser() {
+//		List<User> userList = new ArrayList<User>();
+//		for (int i = 0; i <10; i++) {
+//			userList.add(new User(i,"张三"+i,20+i,"中国广州"));
+//		}
+//
+//		model.addAttribute("users", userList);
+		return "/user/list";
+	}
+	
+//	@RequestMapping("/list")
+//	public String  listUser(Model model) {
+//		List<User> userList = new ArrayList<User>();
+//		for (int i = 0; i <10; i++) {
+//			userList.add(new User(i,"张三"+i,20+i,"中国广州"));
+//		}
+//
+//		model.addAttribute("users", userList);
+//		return "/user/list";
+//	}
+}
